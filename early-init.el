f3a62255f93d44171f50a268aa9efaa33f8a3a0f
Steven Allen <steven@stebalien.com>
2020-07-05 12:33:47 -0700

    prefer newer as early as possible

--- early-init.el
+++ early-init.el
@@ -21,4 +21,6 @@
 ;; Earlier Emacs versions do not load the early init file and do not initialize
 ;; the package manager before loading the init file, so this file is neither
 ;; needed nor loaded on those versions.
-(setq package-enable-at-startup nil)
+(setq
+ package-enable-at-startup nil
+ load-prefer-newer t)
